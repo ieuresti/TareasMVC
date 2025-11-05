a7d9d4aec18532347fe7648065934a0a87278860
Ivan Uresti <talyfuantos@gmail.com>
2025-11-05 16:44:05 -0600

    Agregar archivos de proyecto.

---
+++ TareasMVC/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
